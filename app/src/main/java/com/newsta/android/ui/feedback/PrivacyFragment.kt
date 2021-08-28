26ec2cf8643be193c010998530700c09306a08e6
Somnath6646 <somutalks856@gmail.com>
2021-08-28 17:00:34 +0530

    webview for privacy prolicy and feedback added

---
+++ app/src/main/java/com/newsta/android/ui/feedback/PrivacyFragment.kt
@@ -0,0 +1,44 @@
+package com.newsta.android.ui.feedback
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.navigation.fragment.findNavController
+import com.newsta.android.R
+import com.newsta.android.databinding.FragmentFeedbackBinding
+import com.newsta.android.databinding.FragmentPrivacyBinding
+import com.newsta.android.ui.base.BaseFragment
+
+
+class FeedBackFragment : BaseFragment<FragmentFeedbackBinding>() {
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        // Inflate the layout for this fragment
+
+        binding.webview.webViewClient = WebViewClient()
+
+        // this will load the url of the website
+        val url = "http://www.newsta.in/feedback"
+        binding.webview.loadUrl(url)
+
+        // this will enable the javascript settings
+        binding.webview.settings.javaScriptEnabled = true
+
+        // if you want to enable zoom feature
+        binding.webview.settings.setSupportZoom(true)
+
+        binding.back.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+    }
+
+    override fun getFragmentView(): Int =  R.layout.fragment_feedback
+
+}
